4faf01bcd8a233a47c7f52722a41e12c69909fbe
jelenazj <44589316+jelenazj@users.noreply.github.com>
2019-06-18 15:34:36 +0200

    index.php
    Log in page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log In</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="login">
+		<h1>LOG IN</h1>
+		<form>
+		<input type="text" name="username" placeholder="Enter username"><br/>
+		<input type="password" name="password" placeholder="Enter password"><br/>
+		<input type="submit" class="submit">
+		</form>
+	</div>
+</body>
+</html>
